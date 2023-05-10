e0be91d3eb67a84288674fbaddf890aa1f7f5771
Bohdan Denysenko <denysenko.w0@gmail.com>
2023-05-10 13:37:09 +0300

    Fixed async 'bookUrl' and 'books' acquiring

--- src/Reader.jsx
+++ src/Reader.jsx
@@ -2,39 +2,43 @@ import "./reader.css";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-const Reader = ({ isLoading, books }) => {
+const Reader = ({ books }) => {
   const { id } = useParams();
   const [errMsg, setErrMsg] = useState(null);
+  const [bookUrl, setBookUrl] = useState(null);
 
   useEffect(() => {
     const fetchXhtmlDocument = async () => {
       try {
-        let url;
-        books.forEach((book) =>
-          id === "" + book.id ? (url = book.book_src) : null
-        );
-        const response = await fetch(url);
-        if (!response.ok) throw Error("Retrieving failed.");
-        const xhtml = await response.text();
-        const parser = new DOMParser();
-        const xmlFile = await parser.parseFromString(
-          xhtml,
-          "application/xhtml+xml"
-        );
-        const xmlBody = await xmlFile.getElementsByTagName("body")[0];
-        document.getElementById("r-reader").innerHTML = xmlBody.innerHTML;
+        if (books) {
+          books.forEach((book) =>
+            id === "" + book.id ? setBookUrl(book.book_src) : null
+          );
+        }
+        if (bookUrl) {
+          const response = await fetch(bookUrl);
+          if (!response.ok) throw Error("Retrieving failed.");
+          const xhtml = await response.text();
+          const parser = new DOMParser();
+          const xmlFile = await parser.parseFromString(
+            xhtml,
+            "application/xhtml+xml"
+          );
+          const xmlBody = await xmlFile.getElementsByTagName("body")[0];
+          document.getElementById("r-reader").innerHTML = xmlBody.innerHTML;
+        }
       } catch (err) {
         setErrMsg(err.message);
       }
     };
 
     fetchXhtmlDocument();
-  }, [books]);
+  }, [books, bookUrl]);
 
   return (
     <div id="r-reader" style={{ padding: "20px" }}>
-      {isLoading && <div>Loading...</div>}
-      {!isLoading && errMsg ? errMsg && <p>{errMsg}</p> : null}
+      {!books ? <div>Loading...</div> : null}
+      {errMsg && books ? errMsg && <p>{errMsg}</p> : null}
     </div>
   );
 };
