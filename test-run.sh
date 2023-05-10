329d56ee28a083eefd0b682b8f942105f29323fc
Bohdan Denysenko <denysenko.w0@gmail.com>
2023-05-10 23:59:42 +0300

    Updated 'test-run.sh' file.

--- test-run.sh
+++ test-run.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
 sudo npm run dev &
-sudo npx json-server -p 5174 -w data/books.json &
-sudo http-server -a localhost -p 5175 --cors &
+sudo http-server data/book_unpck/ -a localhost -p 5175 --corssudo npx json-server data/books.json -H localhost -p 5174 --cors
